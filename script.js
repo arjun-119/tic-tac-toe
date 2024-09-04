e8ed351d1887747e0b69871a662793521c005294
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-09-04 17:03:37 +0530

    Create a function to mark a players token .

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
-function Gameboard(){
-    const rows = 3;
-    const columns = 3;
-    board = [];
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  board = [];
 
-    for (let i = 0;i<rows;i++){
-        board[i] = [];
-        for (let j = 0; j<columns;j++){
-            board[i].push(Cell());
-        }
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
     }
+  }
 
+  const addMarker = (row, column, player) => {
+    const availableCells = board[row].filter((item) => item.getValue() === 1);
+
+    if (!availableCells.length) return;
+    board[row][column].markValue(player);
+  };
 }
 
-function Cell(){
-    let value = 1;
+function Cell() {
+  let value = 1;
 
-    const getValue = ()=> value;
+  const getValue = () => value;
 
-    const markValue = (player) => value = player;
+  const markValue = (player) => (value = player);
 
-    return{
-        getValue,
-        markValue
-    };
+  return {
+    getValue,
+    markValue,
+  };
 }
 
-function GameController(){
-
-    const Players = [
-        {
-            name: "Player 1",
-            marker: "X"
-        },
-        {
-            name: "Player 2",
-            marker: "O"
-        }
-    ];
+function GameController() {
+  const Players = [
+    {
+      name: "Player 1",
+      marker: "X",
+    },
+    {
+      name: "Player 2",
+      marker: "O",
+    },
+  ];
 }
